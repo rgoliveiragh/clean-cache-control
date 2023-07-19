ca707cf35446bad6a16636a71dbcad7424b198bd
rgoliveira <rgoliveira1441973@gmail.com>
2023-07-19 17:20:29 -0300

    ensure LocalSavePurchases throws if CacheStore.insert throws

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -23,6 +23,10 @@ class CacheStoreSpy implements CacheStore {
   simulateDeleteError(): void {
     jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => { throw new Error() })
   }
+
+  simulateInsertError(): void {
+    jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => { throw new Error() })
+  }
 }
 
 const mockPurchases = (): Array<SavePurchases.Params> => [{
@@ -79,5 +83,12 @@ describe('LocalSavePurchases', () => {
     expect(cacheStore.insertCallsCount).toBe(1)
     expect(cacheStore.insertKey).toBe('purchases')
     expect(cacheStore.insertValues).toEqual(purchases)
+  })
+  
+  test('Should throw if insert throws', async () => {
+    const { cacheStore, sut } =  makeSut()
+    cacheStore.simulateDeleteError()
+    const promise = sut.save(mockPurchases())
+    expect(promise).rejects.toThrow()
   })
 })
